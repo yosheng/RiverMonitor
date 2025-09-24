46fa6260f4c48bebe26ee5781e9fb3cefa8361b7
旻皇 陳 <tim@egit.com.tw>
2025-09-24 14:15:05 +0800

    真的不分一頁嗎0.0

---
+++ src/RiverMonitor.Api/Controllers/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RiverMonitor.Api.Controllers
+{
+    public class DashBoardController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/RiverMonitor.Api/wwwroot/js/map/ws/client-sr.js
@@ -0,0 +1,14 @@
+﻿// SignalR Initialization
+const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")
+    .withAutomaticReconnect()
+    .build();
+
+connection.start()
+    .then(() => console.log("SignalR connected"))
+    .catch(err => console.error("SignalR connection error:", err));
+
+connection.on("ReceiveMessage", function (user, message) {
+    console.log(`[SignalR] ${user}: ${message}`);
+});
+
