7c3e4e454312b5a5c69e7bc9495fa29e41a2eb0a
yosheng <sad445566@gmail.com>
2025-09-27 21:37:46 +0800

    feat(db): 添加系統設置表及農業數據接口地址配置

---
+++ src/RiverMonitor.Dal/Migrations/20250927133514_AddMoaApiUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RiverMonitor.Dal.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMoaApiUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "SystemSettings",
+                columns: new[] { "Id", "Description", "Key", "Value" },
+                values: new object[] { 3, null, "Endpoint:MoaApi", "https://data.moa.gov.tw" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "SystemSettings",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
